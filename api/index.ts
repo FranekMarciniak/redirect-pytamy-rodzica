9dc7e9bfa555febf595ab2828ec9eba666503b02
Franek Marciniak <ProstyFranek@protonmail.com>
2025-05-31 11:33:45 +0200

    Redirect init

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,10 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.url === '/') {
+    return res.redirect(301, 'https://www.example.com');
+  }
+  
   const { name = 'World' } = req.query;
   return res.json({
     message: `Hello ${name}!`,
